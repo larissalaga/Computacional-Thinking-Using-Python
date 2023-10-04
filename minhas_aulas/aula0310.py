ea11548a58d1ed6f55d53e53a501878f6efd8fc0
larissalaga <larissa.laga@gmail.com>
2023-10-04 08:04:08 -0300

    aula 03/10

---
+++ minhas_aulas/aula0310.py
@@ -0,0 +1,39 @@
+#numeros = [1, 2, 3, 4, 5]
+#for numero in numeros:
+#  print(numero)
+
+#i = 0
+#while numero < 5:
+#  print(numero[i])
+#  i +=1
+
+
+#print range 5
+#numeros = [1, 2, 3, 4, 5]
+
+#for i in range(55, 10, 2):
+#  print(i)
+
+#for i in range(3):
+ # for j in ["a","b"]:
+  #  print (i, j)
+
+
+#def multiplica_matriz(A, B):
+ # resultado = [[0,0], [0,0]]
+  #for i in range(len(A)):
+   # for j in range(len(B)):
+    #  for k in range(len(B)):
+     #   resultado[i][j] += A[i][k]*B[k][j]
+  #return resultado
+
+#A = [[1, 2], [3,4]]
+#B = [[2, 0], [1,3]]
+#C = multiplica_matriz(A,B)
+#print(C)
+
+for i in range(20):
+  for j in range(20):
+    if j % 2 == 0:
+      escrito = [*]
+    print("*")
---
+++ minhas_aulas/aula0310_listcomprehension.py
@@ -0,0 +1,8 @@
+quadrados = [x**2 for x in range(5)]
+
+print(quadrados)
+
+for x in rang(5):
+  if x % 2 == 0:
+    f.append(x**2)
+

ea0d981785f6b005cdd3e1202ab019160b7b859d
larissalaga <larissa.laga@gmail.com>
2023-10-08 22:30:49 -0300

    Challenge

---
+++ minhas_aulas/Challenge.py
@@ -0,0 +1,22 @@
+def opcoes():
+  opcao_escolhida = 1
+  return opcao_escolhida
+
+def cadastro_empresa():
+  cliente =[
+    {"nome": "", }
+  ]
+  return cliente
+
+
+
+
+def menu():
+  opcao_escolhida = opcoes()
+  empresa = cadastro_empresa()
+
+
+    
+        
+if __name__ == "__main__":
+    menu()
